56b0b9f76eb2e22494b4a769404c1df19c05e7a4
Kevin P Murphy <murphyk@gmail.com>
2022-10-28 12:21:17 -0700

    make notebooks work with new repo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
     requirements = list(map(lambda x: x.strip(), f.read().strip().splitlines()))
 
 setup(
-    name="dynamax",
+    name="sts_jax",
     version="0.1",
     description="JAX code for structural time series",
     url="https://github.com/probml/sts-jax",
